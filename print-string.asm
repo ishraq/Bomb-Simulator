37e08f319994c4fde49afaa8627964c52d76cfea
Ishraq <gongy@con.gee>
2016-05-20 15:25:20 +1000

    change indent

--- print-string.asm
+++ print-string.asm
@@ -1,27 +1,26 @@
 .cseg
-    ;takes in address to program memory in r17:r16
-    ;prints string to lcd
-    print_string:
-        ;pre
-        push r16
-        push zl
-        push zh
-        
-        mov zl, r16
-        mov zh, r17
+;takes in address to program memory in r17:r16
+;prints string to lcd
+print_string:
+    ;pre
+    push r16
+    push zl
+    push zh
+    
+    mov zl, r16
+    mov zh, r17
 
-        print_string_loop:
-            lpm r16, z+
-            cpi r16, 0
-            breq print_string_loop_end
-            do_lcd_data_r16
-            rjmp print_string_loop
-        print_string_loop_end:
-                
-        ;post
-        pop zh
-        pop zl
-        pop r16
+    print_string_loop:
+        lpm r16, z+
+        cpi r16, 0
+        breq print_string_loop_end
+        do_lcd_data_r16
+        rjmp print_string_loop
+    print_string_loop_end:
+            
+    ;post
+    pop zh
+    pop zl
+    pop r16
 
-        ret
-
+    ret
